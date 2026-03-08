c16089768c1de474f60e90d7f240c462e05cae83
BockyMen <129232559+BockyMen@users.noreply.github.com>
2026-03-08 10:23:06 -0500

    arreglo final

--- flutter_application_home/lib/widgets/CustomCard.dart
+++ flutter_application_home/lib/widgets/CustomCard.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomCard extends StatelessWidget{
+class CustomCard extends StatelessWidget {
   final String title;
   final String subtitle;
   final bool isSelected;
@@ -12,23 +12,100 @@ class CustomCard extends StatelessWidget{
     required this.subtitle,
     required this.isSelected,
     required this.imagen,
-  }); 
+  });
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
-      color: isSelected ? const Color.fromARGB(31, 99, 99, 99) : const Color.fromARGB(100, 15, 16, 20),
+      color: isSelected
+          ? const Color.fromARGB(31, 99, 99, 99)
+          : const Color.fromARGB(100, 15, 16, 20),
       child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(imagen),
-        ),
+        leading: CircleAvatar(backgroundImage: NetworkImage(imagen)),
         title: Text(
           title,
-          style: const TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF868690)),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF868690),
+          ),
         ),
         subtitle: Text(subtitle),
         trailing: const Icon(Icons.close),
+      ),
+    );
+  }
+}
+
+class UserNotification extends StatelessWidget {
+  final String imagen;
+  final String title;
+  final String title2;
+
+  const UserNotification({
+    super.key,
+    required this.imagen,
+    required this.title,
+    required this.title2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(80, 99, 99, 99),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Container(
+              width: 70,
+              height: double.infinity, // Este se utiliza para que la imagen ocupe todo el espacio del widghet, no se como sirve exactamente, pero lo funciono xd.
+              margin: const EdgeInsets.only(right: 12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage(imagen),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded( // Se utiliza para que los textos ocupen todo el espacio disponible que tienen
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    title2,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Icon(Icons.volume_up, color: Colors.white),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
