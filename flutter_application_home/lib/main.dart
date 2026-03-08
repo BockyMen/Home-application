a749aea5d6ee380408689bfc4295521820a60c86
AxdaTFK <axldug5@gmail.com>
2026-03-08 00:21:54 -0500

    Update main.dart

--- flutter_application_home/lib/main.dart
+++ flutter_application_home/lib/main.dart
@@ -40,7 +40,6 @@ class MainApp extends StatelessWidget {
                 children: [
                   UserNotification(imagen: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg", title: 'SebitasV y hide en lan', title2: 'General'),
                   UserNotification(imagen: "https://i.pinimg.com/474x/74/bb/34/74bb340ffe87e31837a04a538f1bbc10.jpg", title: 'Osvaldo', title2: 'Online'),
-                  UserNotification(imagen: "", title: 'BockyMen', title2: 'Manqueando en terror')
                 ],
               ),
             ),
