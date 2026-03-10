838d407e571387443aada16e9b8f5e73ae6f2930
J <110360863+J24141@users.noreply.github.com>
2026-03-09 21:08:38 -0500

    FlutterAplication_Julian1

--- flutter_application_home/lib/main.dart
+++ flutter_application_home/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_home/widgets/Barralateral.dart';
 import './widgets/CustomCard.dart';
 
 void main() {
@@ -12,156 +13,280 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color.fromARGB(150, 15, 16, 20),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(97, 15, 16, 20),
-          toolbarHeight: 100,
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+
+        body: Padding(padding: 
+        EdgeInsetsGeometry.directional(top: 40),
+        
+        child: Row(children: [
+          Container(
+            padding: EdgeInsets.all(5),
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+          child: ListView(
+            padding: EdgeInsets.zero,
             children: [
-              Text("tiempos", style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 200, 200, 207), fontWeight: FontWeight.w500, fontFamily: "Discord")),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(57, 134, 134, 144),
-                      borderRadius: BorderRadius.circular(20)
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 95, 119, 213),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Icon(Icons.messenger_sharp, color: Colors.white),
                     ),
-                    child: Icon(Icons.search_rounded,color: Color.fromARGB(255, 169, 169, 180)),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 95, vertical: 5),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(57, 134, 134, 144),
-                      borderRadius: BorderRadius.circular(30)
+
+                    // 👇 Línea debajo del ícono
+                    const Divider(
+                      color: Colors.white24,
+                      thickness: 1,
+                      height: 20,
+                      indent: 8,
+                      endIndent: 8,
                     ),
-                    child: Row( 
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.person_add,color: Color.fromARGB(255, 181, 181, 192), size: 25,),
-                        SizedBox(width: 8),
-                        Text("Añadir amigos", style: TextStyle(fontSize: 15, color: Color.fromARGB(255, 255, 255, 255), fontFamily: "Discord", fontWeight: FontWeight.bold))
-                      ],
-                    )
-                  ),
-                ],
+                  ],
+                ),
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
+              ),
+              barraLateralFija(
+                image: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg",
+                isMessaged: true,
+                message: 3,
               ),
             ],
           ),
-        ),
-        body: ListView(
-          children: [
-            SizedBox(
-              height: 110,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  UserNotification(imagen: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg", title: 'SebitasV y hide en lan', title2: 'General'),
-                  UserNotification(imagen: "https://i.pinimg.com/474x/74/bb/34/74bb340ffe87e31837a04a538f1bbc10.jpg", title: 'Osvaldo', title2: 'Online'),
-                  UserNotification(imagen: "https://yt3.googleusercontent.com/lDfhM9v8EMD9xu5AySo8KXDP0PwajfJowF66T3Kmte6mLl69okvXnFav3zWr01mO7jNonq19-Ow=s160-c-k-c0x00ffffff-no-rj", title: 'BockyMen', title2: 'Manqueando en terror')
-                ],
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(150, 20, 20, 20),
+              borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16),
               ),
             ),
-            CustomCard(
-              title: "Minecraft",
-              subtitle: "PvP 1.8.9 la mejor versión",
-              isSelected: true,
-              imagen:
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Minecraft-creeper-face.svg/1280px-Minecraft-creeper-face.svg.png",
-              tiempo: "5h",
-            ),
-            CustomCard(
-              title: "Kirby",
-              subtitle: "Facherisimo",
-              isSelected: false,
-              imagen:
-                  "https://pbs.twimg.com/profile_images/1512424162505150469/1iqopIbB_400x400.jpg",
-              tiempo: "5h",
-            ),
-            CustomCard(
-              title: "Gato",
-              subtitle: "Arabe",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/564x/7e/0e/cd/7e0ecd983170fb2e26fe2f9ec3b70058.jpg",
-              tiempo: "18h",
-            ),
-            CustomCard(
-              title: "Gato",
-              subtitle: "Whiskas",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/474x/7b/1d/52/7b1d526899529bdae2c811e52cee7141.jpg",
-              tiempo: "20h",
-            ),
-            CustomCard(
-              title: "Erick",
-              subtitle: "Sin vida",
-              isSelected: false,
-              imagen:
-                  "https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/5NEPHCKSG5G6PA3KARK6TVWGZU.jpg",
-              tiempo: "1 días",
-            ),
-            CustomCard(
-              title: "Dios de la ofertas",
-              subtitle: "La mejor inversión",
-              isSelected: false,
-              imagen:
-                  "https://i.blogs.es/ea7b9d/rebajassteam2017/1366_2000.jpg",
-              tiempo: "3 días",
-            ),
-            CustomCard(
-              title: "Buzz",
-              subtitle: "Al infinito y más alla",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/1200x/65/a4/0e/65a40e2ae16e775c8c7a8fa977ac4f39.jpg",
-              tiempo: "6 días",
-            ),
-            CustomCard(
-              title: "Pablo",
-              subtitle: "El chisme es vida",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/1200x/02/13/0c/02130c599d2b8c0271c4d970d32e67bf.jpg",
-              tiempo: "12 días",
-            ),
-            CustomCard(
-              title: "Pato",
-              subtitle: "Peguelo",
-              isSelected: false,
-              imagen:
-                  "https://wallpapers.com/images/featured/imagenes-de-perfil-de-meme-vnigweuy4onsxunv.jpg",
-              tiempo: "20 días",
-            ),
-            CustomCard(
-              title: "Pajaro Esquizo",
-              subtitle: "Donde estan las womans",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/236x/1b/e9/19/1be91948bb228ebc114f0e456b1c08f5.jpg",
-              tiempo: "27 días",
-            ),
-            CustomCard(
-              title: "Shrek",
-              subtitle: "Salgan de mi pantano!",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/474x/71/92/76/719276fb3ef2363ad1d4a6cb39e7530a.jpg",
-              tiempo: "1 mes(es)",
-            ),
-            CustomCard(
-              title: "Rosa",
-              subtitle: "Cuidadito",
-              isSelected: false,
-              imagen:
-                  "https://i.pinimg.com/474x/6a/c6/ef/6ac6ef1ff804d249c09e155fd80288a0.jpg",
-              tiempo: "3 mes(es)",
-            ),
-          ],
-        ),
+
+            child: Column(children: [
+              Container(
+              color: const Color.fromARGB(97, 15, 16, 20),
+              height: 100,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Mensajes", style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 200, 200, 207), fontWeight: FontWeight.w500, fontFamily: "Discord")),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(57, 134, 134, 144),
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Icon(Icons.search_rounded,color: Color.fromARGB(255, 169, 169, 180)),
+                        ),
+                        const SizedBox(width: 10),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(horizontal: 55, vertical: 5),
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(57, 134, 134, 144),
+                                borderRadius: BorderRadius.circular(30)
+                              ),
+                                child: Row( 
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(Icons.person_add,color: Color.fromARGB(255, 181, 181, 192), size: 25,),
+                                    SizedBox(width: 8),
+                                    Text("Añadir amigos", style: TextStyle(fontSize: 15, color: Color.fromARGB(255, 255, 255, 255), fontFamily: "Discord", fontWeight: FontWeight.bold))
+                                ],
+                              ),
+                          ),
+                      
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ),
+
+              Expanded(
+            child: ListView(
+              children: [
+
+                SizedBox(
+                  height: 110,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      UserNotification(imagen: "https://i.pinimg.com/474x/a7/de/c4/a7dec46aca07b2e12e2e361cfa9312ec.jpg", title: 'SebitasV y hide en lan', title2: 'General'),
+                      UserNotification(imagen: "https://i.pinimg.com/474x/74/bb/34/74bb340ffe87e31837a04a538f1bbc10.jpg", title: 'Osvaldo', title2: 'Online'),
+                      UserNotification(imagen: "https://yt3.googleusercontent.com/lDfhM9v8EMD9xu5AySo8KXDP0PwajfJowF66T3Kmte6mLl69okvXnFav3zWr01mO7jNonq19-Ow=s160-c-k-c0x00ffffff-no-rj", title: 'BockyMen', title2: 'Manqueando en terror')
+                    ],
+                  ),
+                ),
+                CustomCard(
+                  title: "Minecraft",
+                  subtitle: "PvP 1.8.9 la mejor versión",
+                  isSelected: true,
+                  imagen:
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Minecraft-creeper-face.svg/1280px-Minecraft-creeper-face.svg.png",
+                  tiempo: "5h",
+                ),
+                CustomCard(
+                  title: "Kirby",
+                  subtitle: "Facherisimo",
+                  isSelected: false,
+                  imagen:
+                      "https://pbs.twimg.com/profile_images/1512424162505150469/1iqopIbB_400x400.jpg",
+                  tiempo: "5h",
+                ),
+                CustomCard(
+                  title: "Gato",
+                  subtitle: "Arabe",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/564x/7e/0e/cd/7e0ecd983170fb2e26fe2f9ec3b70058.jpg",
+                  tiempo: "18h",
+                ),
+                CustomCard(
+                  title: "Gato",
+                  subtitle: "Whiskas",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/474x/7b/1d/52/7b1d526899529bdae2c811e52cee7141.jpg",
+                  tiempo: "20h",
+                ),
+                CustomCard(
+                  title: "Erick",
+                  subtitle: "Sin vida",
+                  isSelected: false,
+                  imagen:
+                      "https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/5NEPHCKSG5G6PA3KARK6TVWGZU.jpg",
+                  tiempo: "1 días",
+                ),
+                CustomCard(
+                  title: "Dios de la ofertas",
+                  subtitle: "La mejor inversión",
+                  isSelected: false,
+                  imagen:
+                      "https://i.blogs.es/ea7b9d/rebajassteam2017/1366_2000.jpg",
+                  tiempo: "3 días",
+                ),
+                CustomCard(
+                  title: "Buzz",
+                  subtitle: "Al infinito y más alla",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/1200x/65/a4/0e/65a40e2ae16e775c8c7a8fa977ac4f39.jpg",
+                  tiempo: "6 días",
+                ),
+                CustomCard(
+                  title: "Pablo",
+                  subtitle: "El chisme es vida",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/1200x/02/13/0c/02130c599d2b8c0271c4d970d32e67bf.jpg",
+                  tiempo: "12 días",
+                ),
+                CustomCard(
+                  title: "Pato",
+                  subtitle: "Peguelo",
+                  isSelected: false,
+                  imagen:
+                      "https://wallpapers.com/images/featured/imagenes-de-perfil-de-meme-vnigweuy4onsxunv.jpg",
+                  tiempo: "20 días",
+                ),
+                CustomCard(
+                  title: "Pajaro Esquizo",
+                  subtitle: "Donde estan las womans",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/236x/1b/e9/19/1be91948bb228ebc114f0e456b1c08f5.jpg",
+                  tiempo: "27 días",
+                ),
+                CustomCard(
+                  title: "Shrek",
+                  subtitle: "Salgan de mi pantano!",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/474x/71/92/76/719276fb3ef2363ad1d4a6cb39e7530a.jpg",
+                  tiempo: "1 mes(es)",
+                ),
+                CustomCard(
+                  title: "Rosa",
+                  subtitle: "Cuidadito",
+                  isSelected: false,
+                  imagen:
+                      "https://i.pinimg.com/474x/6a/c6/ef/6ac6ef1ff804d249c09e155fd80288a0.jpg",
+                  tiempo: "3 mes(es)",
+                ),
+              ],
+            ),),
+            ],
+          ))),
+          
+          
+        ],
+        ),),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: 0,
           selectedItemColor: const Color(0xffffffff),
--- flutter_application_home/lib/widgets/Barralateral.dart
+++ flutter_application_home/lib/widgets/Barralateral.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
 
-class Barralateral extends StatelessWidget{
-  final IconData icon;
-  final Color? backgroundColor;
+class barraLateralFija extends StatelessWidget {
+  final String image;
+  final bool isMessaged;
+  final int message;
 
-  const Barralateral({
+  const barraLateralFija({
     super.key,
-    required this.icon,
-    required this.backgroundColor,
-  }); 
+    required this.image,
+    required this.isMessaged,
+    required this.message,
+  });
 
-@override
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      color: backgroundColor,
-      child: ListTile(
-        leading: Icon(icon, size: 32),
-      ),
+    return Column(
+      children: [
+        Stack(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              backgroundImage: NetworkImage(image),
+            ),
+            if (isMessaged)
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(2),
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 18,
+                    minHeight: 18,
+                  ),
+                  child: Center(
+                    child: Text(
+                      message.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
     );
   }
 }
